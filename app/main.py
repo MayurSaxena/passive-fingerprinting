42a797b2c4cc4633633137a030b0b11ee546569a
Mayur Saxena <mayur.saxena1997@gmail.com>
2022-07-19 17:12:44 -0400

    Get real IP/port

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ async def read_root(request: Request):
     filename = str(uuid.uuid4())
 
     with open(f'output/{filename}.txt','w') as f:
-        f.write(f"{request.client.host}:{request.client.port}\n")
+        f.write(f"{request.headers.get('X-FP-IP')}:{request.headers.get('X-FP-Port')}\n")
         f.write(f"{request.headers.get('user-agent')}\n")
         for key,value in request.headers.items():
             f.write(f"{key}: {value}\n")
